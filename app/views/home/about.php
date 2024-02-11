fc92eafed7d14d4f2a3cdf65f10e67c210d21c21
rayhanuug <raihannugrah@yahoo.com>
2024-02-11 18:03:43 +0700

    sedang mengerjakan landing why-choose us

--- app/views/home/about.php
+++ app/views/home/about.php
@@ -1,1 +1,15 @@
-    <h1>Halo ini untuk about us</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/styles-about.css">
+    <title>Torindo</title>
+</head>
+<body>
+
+    <h1><i>Why </i>choose us?</h1>
+        
+        
+</body>
+</html>
