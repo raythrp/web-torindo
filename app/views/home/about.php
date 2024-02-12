db33e3c56923bd9880ab6bfdbe23c19444eb0bf5
raythrp <rayhanaprasnaputra@gmail.com>
2024-02-12 21:19:38 +0700

    Landing page (Hero Done)

--- app/views/home/about.php
+++ app/views/home/about.php
@@ -1,2 +1,50 @@
+<link rel="stylesheet" href="public/css/styles-landing.css">
 
-    <h1>Halo ini untuk about us</h1>
+    <header>
+        <nav>
+            <h1 class="logo">Torindo Jaya <br>Persada</h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Our Product</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <div class="hero">
+            <img src="public/img/aboutHero.png" alt="">
+            <div class="motto">
+                <p>So, you're interested?</p>
+                <h1>Lets get <span>Deeper.</span></h1>
+                <p class="scroll">Scroll down to see more!</p>
+            </div>
+        </div>
+        <div class="vision">
+            <div class="title">
+                <h1>Torindo's Vision</h1>
+            </div>
+            <div class="vision-motto">
+                <img src="" alt="">
+                <p>The whole set of <span>solutions</span> provided to create <span>greater</span> value for customers</p>
+            </div>
+            <div class="vision-achieve">
+                <div class="top">
+                    <div class="big">
+                        <h1>to Achieve</h1>
+                    </div>
+                    <div class="med">
+                       <p>Torindo Jaya Persada strives to commit in <span>“Serving Better Support”</span></p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <p>We will constantly work to be a reliable partner who adds real value to the businesses of our customer</p>
+                </div>
+            </div>
+            <div class="vision-goal">
+                <h1>The Ultimate <span>GOAL</span></h1>
+                <p>Achieve consistent business growth and profitability. We accomplish this by developing lasting partnerships with our customers, taking advantage of the latest technological advances and being a good corporate citizen.</p>
+            </div>
+        </div>
+    </main>
