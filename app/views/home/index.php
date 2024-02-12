c2b7a3b7b2665540cc88bb9ec61a10c7a492b5b6
rayhanuug <raihannugrah@yahoo.com>
2024-02-12 16:01:54 +0700

    on progress: why choose us(90%)

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,22 @@
 <link rel="stylesheet" href="public/css/styles-about.css"> <!-- Line 1 untuk include css -->
-    <h1><i>Why </i>choose us?</h1>
+        <h1><i>Why </i>choose us?</h1>
+        <div class="container-box">
+            <div class="biggest-box">
+                <p><span>No 1 BIGGEST </span>population China product in</p>
+            </div>
+        </div>
+        <div class="years">
+                <p id="ten">10+</p>
+                <p id="text1"><span id="span1">Years</span> experience <br> Professional manpower to support <span id="span2">YOUR</span> business</p>
+         </div>
+         <div class="global">
+                <h2>We reach <span id="span1">GLOBALLY<span></h2>
+                <p>Maintains a <span id="span2">VAST</span> operation to serve our customers</p>
+         </div>
+         <div class="invest">
+                <h2>INVEST</h2>
+                <p>continuosly in developing our team to ensure giving <span>DIFFERENT TASTE</span> of After Sales Support Service to the Customers</p>
+         </div>
+        
+
 <!-- Line terakhir untuk include javasript *kalau ada -->
