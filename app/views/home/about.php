892b8bd03bbca083747eb639d7ab8a793a3ff74d
raythrp <rayhanaprasnaputra@gmail.com>
2024-02-14 17:49:01 +0700

    About Us (only core values left), footer minor css build

--- app/views/home/about.php
+++ app/views/home/about.php
@@ -23,11 +23,11 @@
         </div>
 
         <div class="vision">
-            <div class="title">
-                <h1>Torindo's Vision</h1>
+            <div class="vision-title">
+                <h1><span>Torindo's</span> Vision</h1>
             </div>
             <div class="vision-motto">
-                <img src="" alt="">
+                <img src="public/img/aboutHero.png" alt="">
                 <p>The whole set of <span>solutions</span> provided to create <span>greater</span> value for customers</p>
             </div>
             <div class="vision-achieve">
@@ -36,7 +36,7 @@
                         <h1>to Achieve</h1>
                     </div>
                     <div class="med">
-                       <p>Torindo Jaya Persada strives to commit in <span>“Serving Better Support”</span></p>
+                       <p>Torindo Jaya Persada strives to commit in  <span>“Serving Better Support”</span></p>
                     </div>
                 </div>
                 <div class="bottom">
@@ -44,12 +44,25 @@
                 </div>
             </div>
             <div class="vision-goal">
-                <h1>The Ultimate <span>GOAL</span></h1>
-                <p>Achieve consistent business growth and profitability. We accomplish this by developing lasting partnerships with our customers, taking advantage of the latest technological advances and being a good corporate citizen.</p>
+                <div class="big">
+                    <h1>The Ultimate <span>GOAL</span></h1>
+                </div>
+                <div class="content">
+                    <p>Achieve consistent business growth and profitability. We accomplish this by developing lasting partnerships with our customers, taking advantage of the latest technological advances and being a good corporate citizen.</p>
+                </div>
             </div>
         </div>
 
         <div class="achievements">
-            
+            <div class="title">
+                <h1><span>Torindo's</span> Achievements</h1>
+            </div>
+            <div class="content">
+                <p><span>10+</span> Years experience Professional manpower to support your business</p>
+                <p><span>Biggest</span> population China product in Indonesia</p>
+                <p><span>We Reach</span> <strong>Globally</strong> maintains a <strong>vast</strong> operation to serve our customers</p>
+                <p>With an area based organization comprising of <span>Sumatra, Kalimantan, and Sulawesi.</span> We provide the best solution for all customers</p>
+                <p><span>Invest</span> continuosly in developing our team to ensure giving <strong>different taste of after sales</strong> support service to the customers</p>
+            </div>
         </div>
     </main>
--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,2 +1,36 @@
+<link rel="stylesheet" href="public/css/styles-footer.css">
+    
+    <footer>
+        <div class="content">
+            <div class="profile">
+                <img src="" alt="">
+                <div class="title">
+                    <h1>Torindo Jaya Persada</h1>
+                </div>
+                <div class="address">
+                    <p>Jl. Torindo Makmur 99, Kelurahan</p>
+                    <p>Kecamatan, kota, Provinsi KodePos</p>
+                </div>
+            </div>
+            <div class="contact">
+                <div class="email">
+                    <h1>Email</h1>
+                    <p>support@torindojaya.com</p>
+                </div>
+                <div class="phone">
+                    <h1>Phone</h1>
+                    <p>+62 811-880-959</p>
+                    <p>+62 8111-334-877</p>
+                </div> 
+            </div>
+            <div class="nav">
+                <p>Home</p>
+                <p>About Us</p>
+                <p>Our Product</p>
+                <p>Contact Us</p>
+            </div>
+        </div>
+        <p class="copy">&copy; 2024 Torindo Jaya Persada</p>
+    </footer>
 </body>
 </html>
