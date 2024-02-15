1791cb5da402ab6b1c0295e6444b23ee78b92fb4
rayhanuug <raihannugrah@yahoo.com>
2024-02-15 23:36:10 +0700

    In Progress : Products (30%)

--- app/views/home/products.php
+++ app/views/home/products.php
@@ -6,14 +6,36 @@
                 <p class="scroll">Scroll down to see more!</p>
             </div>
         </div>
+
         <div class="page1">
             <div class="header1">
-                <img src="public/icon-img/helmet.svg" alt="helmet"/>
+                <img src="public/icon/helmet.svg" alt="helmet"/>
                 <p><span>Mining</span> Products</p>
             </div>
             <div class="isi1">
-                    <img src="" alt="">
-                    <h1><span id="h1">Mining</span> Truck</h1>
-                    <p><span>Providing you with pristine, state-of-the art</span> dump truck, wide body trucks, and rigid dump truck with proven track records from various mining companies throughout the world</p>
+                <div class="img1">
+                    <img src="public/img/miningTruck.png" alt="miningTruck">
+                </div>
+                <div class="text1">
+                        <h1><span id="h1">Mining</span> Truck</h1>
+                        <p><span>Providing you with pristine, state-of-the art</span> dump truck, wide body trucks, and rigid dump truck with proven track records from various mining companies throughout the world</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="page2">
+            <div class="header1">
+                <img src="public/icon/wrench.svg" alt="helmet"/>
+                <p><span>Parts &</span> services</p>
+            </div>
+            <div class="isi2">
+                <div class="text2">
+                        <h1><span id="h1">Mining</span> Component & Parts</h1>
+                        <p>Brand new, crucial inner working parts with<span> Excellent reabillity & outstanding perform <br>output</span></p>
+                        <p>high-speed engine & gearbox suited for all purpose mining activities. <span>Durable</span> and <span>field-tested</span> differential and brakes available with the best quality.
+                </div>
+                <div class="img2">
+                    <img src="public/img/miningTruck.png" alt="miningTruck">
+                </div>
             </div>
         </div>
