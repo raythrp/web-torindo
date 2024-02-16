cf034f02ad6595fe16eb1d76379f4af3368f74c7
rayhanuug <raihannugrah@yahoo.com>
2024-02-16 15:10:36 +0700

    in progress : Products section (90%)

--- app/views/home/products.php
+++ app/views/home/products.php
@@ -25,8 +25,8 @@
 
         <div class="page2">
             <div class="header1">
-                <img src="public/icon/wrench.svg" alt="helmet"/>
-                <p><span>Parts &</span> services</p>
+                <img src="public/icon/wrench.svg" alt="Services"/>
+                <p><span>Parts &</span> Services</p>
             </div>
             <div class="isi2">
                 <div class="text2">
@@ -38,4 +38,53 @@
                     <img src="public/img/miningTruck.png" alt="miningTruck">
                 </div>
             </div>
+
+            <div class="page1">
+            <div class="header1">
+                <img src="public/icon/user-gear.svg" alt="System & Safety device"/>
+                <p><span>System & </span>Safety device</p>
+            </div>
+            <div class="isi1">
+                <div class="img1">
+                    <img src="public/img/miningTruck.png" alt="miningTruck">
+                </div>
+                <div class="text1">
+                        <p id="p1">The best fire suppression system on the market, widely used for vehicular applications and <span>highly applicable</span> for mining truck usage.</h1>
+                        <p id="p2">Smart Monitoring Systems to ensure <span>maximum efficiency, productivity,</span> and <span>Longevity of units</span></p>
+                </div>
+            </div>
+
+            <div class="page2">
+                <div class="header1">
+                    <img src="public/icon/kaaba.svg" alt="kaaba"/>
+                    <a href="umrah"><span>Umrah Travel & </span>Services</a>
+                </div>
+                <div class="isi2">
+                    <div class="text2">
+                            <h1 id="h2">Journey With <span>Heart</span></h1>
+                            <p id="p2">Pahami alur dan rangkaian <span>Umroh</span> bersama <span>Kami</span></p>
+                    </div>
+                    <div class="img2">
+                        <img src="public/img/miningTruck.png" alt="miningTruck">
+                    </div>
+                </div>
+            </div>
+
+            <div class="page3">
+            <div class="header3">
+                <img src="public/icon/handshake.svg" alt="Recruitment"/>
+                <a href="recruitment"><span>Recruitment </span>Services</a>
+            </div>
+            <div class="isi3">
+                <div class="img3">
+                    <img src="public/img/miningTruck.png" alt="miningTruck">
+                </div>
+                <div class="text3">
+                        <p>EMPOWERING YOUR WORKFORCE, <span>ONE EMPLOYEE AT A TIME</span></p>
+                </div>
+            </div>
+
+
+        </div>
+
         </div>
