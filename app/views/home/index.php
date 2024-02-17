e7868173302df72e3fc2916c4eb8a7ea6387f98d
raythrp <rayhanaprasnaputra@gmail.com>
2024-02-17 23:40:50 +0700

    Landing page css fixes

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -7,51 +7,53 @@
             </div>
         </div>
 
-        <h1><i id="italic">Why </i>choose us?</h1>
-        <div class="container-box">
-            <div class="biggest-box">
-                <p><span>No 1 BIGGEST </span>population China product in</p>
-            </div>
-        </div>
-        <div class="years">
-                <p id="ten">10+</p>
-                <p id="text1"><span id="span1">Years</span> experience <br> Professional manpower to support <span id="span2">YOUR</span> business</p>
-         </div>
-         <div class="global">
-                <h2>We reach <span id="span1">GLOBALLY<span></h2>
-                <p>Maintains a <span id="span2">VAST</span> operation to serve our customers</p>
-         </div>
-         <div class="invest">
-                <h2>INVEST</h2>
-                <p>continuosly in developing our team to ensure giving <span>DIFFERENT TASTE</span> of After Sales Support Service to the Customers</p>
-         </div>
-         <div class="icon1">
-              <div class="icon01">
-                     <img id="icon01" src="public/icon/mechanical.svg" alt="gear"/>
-                     <p>ORIGINAL EQUIPMENT MANUFACTURER</p>
+        <div class="wrapper">
+              <h1 class="italic"><i>Why </i>choose us?</h1>
+              <div class="container-box">
+              <div class="biggest-box">
+                     <p><span>No 1 BIGGEST </span>population China product in</p>
               </div>
-              <div class="icon02">
-                     <img id="icon01" src="public/icon/achievement.svg" alt="achievement"/>
-                     <p>DELIVERING PREMIUM PRODUCTS TO ITS GLOBAL CUSTOMER</p>
               </div>
-              <div class="icon03">
-                     <img id="icon02" src="public/icon/people-group.svg" alt="people group"/>
-                     <p>ASSISTED BY EXPERT ADVISORS</p>
+              <div class="years">
+                     <p id="ten">10+</p>
+                     <p id="text1"><span id="span1">Years</span> experience <br> Professional manpower to support <span id="span2">YOUR</span> business</p>
               </div>
-         </div>
-         <div class="icon2">
-              <div class="icon01">
-                     <img id="icon01" src="public/icon/product.svg" alt="gear"/>
-                     <p>PRODUCT SUPPORT CAPABILITIES</p>
+              <div class="global">
+                     <h2>We reach <span id="span1">GLOBALLY<span></h2>
+                     <p>Maintains a <span id="span2">VAST</span> operation to serve our customers</p>
               </div>
-              <div class="icon02">
-                     <img id="icon01" src="public/icon/phone.svg" alt="achievement"/>
-                     <p>24/7 EASY TO CONTACT US</p>
+              <div class="invest">
+                     <h2>INVEST</h2>
+                     <p>continuosly in developing our team to ensure giving <span>DIFFERENT TASTE</span> of After Sales Support Service to the Customers</p>
               </div>
-         </div>
-        <div class="know">
-              <p><i>What are you waiting for?</i></p>
-              <a id="get" href="about"><b>Get to know us more</b></a>
+              <div class="icon1">
+                     <div class="icon01">
+                            <img id="icon01" src="public/icon/mechanical.svg" alt="gear"/>
+                            <p>ORIGINAL EQUIPMENT MANUFACTURER</p>
+                     </div>
+                     <div class="icon02">
+                            <img id="icon01" src="public/icon/achievement.svg" alt="achievement"/>
+                            <p>DELIVERING PREMIUM PRODUCTS TO ITS GLOBAL CUSTOMER</p>
+                     </div>
+                     <div class="icon03">
+                            <img id="icon02" src="public/icon/people-group.svg" alt="people group"/>
+                            <p>ASSISTED BY EXPERT ADVISORS</p>
+                     </div>
+              </div>
+              <div class="icon2">
+                     <div class="icon01">
+                            <img id="icon01" src="public/icon/product.svg" alt="gear"/>
+                            <p>PRODUCT SUPPORT CAPABILITIES</p>
+                     </div>
+                     <div class="icon02">
+                            <img id="icon01" src="public/icon/phone.svg" alt="achievement"/>
+                            <p>24/7 EASY TO CONTACT US</p>
+                     </div>
+              </div>
+              <div class="know">
+                     <p><i>What are you waiting for?</i></p>
+                     <a id="get" href="about"><b>Get to know us more</b></a>
+              </div>
         </div>
 
 <!-- Line terakhir untuk include javasript *kalau ada -->
